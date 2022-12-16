f14137e28c32ebb9b77114fbe9354c644ede895d
Lucie Lesnier <llesnier@intra.weka.fr>
2022-12-16 14:51:59 +0100

    problems

--- app/src/main/java/com/example/yapasdsou/modules/OnBoardingView.kt
+++ app/src/main/java/com/example/yapasdsou/modules/OnBoardingView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,7 +22,7 @@ Column(modifier = Modifier
     .fillMaxSize()
     .background(color = MaterialTheme.colors.primary)) {
    Column {
-       Text("PAS DE SOUS ?", style= TextStyle(fontFamily= MaterialTheme., fontWeight = FontWeight.Medium, color = Color.White))
+       Text("PAS DE SOUS ?", style= TextStyle(fontWeight = FontWeight.Medium, color = Color.White, fontFamily = typography.h1.fontFamily))
 Text("Pas de sous")
    }
 }

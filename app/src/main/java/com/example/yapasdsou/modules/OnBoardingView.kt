67e91d19971a2e1450574f21a48fb414653aa4d4
Lucie Lesnier <llesnier@intra.weka.fr>
2022-12-16 13:19:50 +0100

    add packages

---
+++ app/src/main/java/com/example/yapasdsou/modules/OnBoardingView.kt
@@ -0,0 +1,42 @@
+package com.example.yapasdsou.modules
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun OnBoardingView() {
+Column(modifier = Modifier
+    .fillMaxSize()
+    .background(color = MaterialTheme.colors.primary)) {
+   Column {
+       Text(  buildAnnotatedString {
+           withStyle(style = ParagraphStyle(lineHeight = 30.sp)) {
+               withStyle(style = SpanStyle(color = Color.White, fontSize = 30.sp)) {
+                   append("PAS DE SOUS ?")
+               }}})
+
+   }
+}
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    OnBoardingView()
+}
--- app/src/main/java/com/example/yapasdsou/ui/theme/Color.kt
+++ app/src/main/java/com/example/yapasdsou/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.yapasdsou.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
+val Purple200 = Color(0xFF5F67EA)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
